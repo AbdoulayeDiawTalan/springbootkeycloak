32b0c3dcdcbd4ab0ce4ee5e5c0426de03561c0b1
AbdoulayeDiaw <abdoulayediaw6@hotmail.com>
2025-01-16 09:28:24 +0100

    Keycloak configuration

---
+++ src/main/java/com/guestu/keycloaksimpleauth/config/SecurityConfig.java
@@ -0,0 +1,52 @@
+package com.guestu.keycloaksimpleauth.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.security.oauth2.resource.OAuth2ResourceServerProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.oauth2.jwt.Jwt;
+import org.springframework.security.oauth2.jwt.JwtDecoder;
+import org.springframework.security.oauth2.jwt.JwtDecoders;
+import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationConverter;
+import org.springframework.security.oauth2.server.resource.authentication.JwtGrantedAuthoritiesConverter;
+import org.springframework.security.web.SecurityFilterChain;
+
+import java.util.Collection;
+import java.util.Collections;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+@Configuration
+public class SecurityConfig {
+
+    @Autowired
+    JwtAuthConverter authConverter;
+
+    @Bean
+    public JwtDecoder jwtDecoder(OAuth2ResourceServerProperties properties) {
+        return JwtDecoders.fromIssuerLocation(properties.getJwt().getIssuerUri());
+    }
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .authorizeHttpRequests(authorize -> authorize
+                        .requestMatchers("/helloAdmin").hasRole("ADMIN")
+                        .requestMatchers("/helloUser").hasRole("USER")
+                        .requestMatchers("/redirect").permitAll()
+                        .requestMatchers("www.google.fr").permitAll()
+                        .anyRequest().authenticated()
+                )
+                .oauth2ResourceServer(oauth2 -> oauth2
+                        .jwt(jwt -> jwt
+                                .jwtAuthenticationConverter(authConverter)
+                        )
+                );
+
+        return http.build();
+    }
+
+
+}
